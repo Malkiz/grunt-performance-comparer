df323ec8c80a9f3843651ccaf3ed3a43ab28fa42
omri <omri.ey@walkme.com>
2015-03-08 09:43:19 +0200

    abort when fail + override option

--- tasks/performance-comparer.js
+++ tasks/performance-comparer.js
@@ -8,6 +8,7 @@ module.exports = function(grunt) {
 	grunt.registerMultiTask('performance-comparer', 'compare performance results', function(){
 		var _this = this;
 		var options = _this.options();
+		var override = grunt.option('override');
 		var xmlFilePaths = _this.files.reduce(function(arr, file) {
 			return arr.concat(file.src);
 		}, []);
@@ -26,6 +27,19 @@ module.exports = function(grunt) {
 		if (options.aggregate) {
 			console.log('Aggregating to ' + options.aggregate);
 			parsedData = comparer.aggregate(parsedData, options.aggregate, options.adaptResultFn);
+		}
+
+		var failure = Object.keys(parsedData).reduce(function (b, filepath) {
+			return b || Object.keys(parsedData[filepath]).reduce(function (b, testsuite) {
+				return b || Object.keys(parsedData[filepath][testsuite].testcases).reduce(function (b, fullName) {
+					return b || parsedData[filepath][testsuite].testcases[fullName].failure;
+				}, false);
+			}, false);
+		}, false);
+
+		if (failure) {
+			console.log('There were failed tests - aborting performance test.');
+			return;
 		}
 
 		var isPrevPrefix = false;
@@ -56,7 +70,7 @@ module.exports = function(grunt) {
 			}
 		} else {
 			isPrevPrefix = true;
-			prev = Object.keys(parsedData).reduce(function(map, filepath) {
+			prev = Object.keys(parsedData).reduce(function (map, filepath) {
 				var filename = filepath.substring(filepath.lastIndexOf('/') + 1, filepath.lastIndexOf('.'));
 				xmlPathToFilename[filepath] = filename;
 				var path = options.prev + filename + '.js';
@@ -124,7 +138,7 @@ module.exports = function(grunt) {
 			console.log('For details see: \n  ' + compareResult.pathsArr.join('\n  '));
 		}
 
-		if (isPrevPrefix && (options.override || Object.keys(prev).length == 0)) {
+		if (isPrevPrefix && (override || options.override || Object.keys(prev).length == 0)) {
 			console.log('Saving prev files:')
 			Object.keys(parsedData).forEach(function(filepath) {
 				var path = options.prev + xmlPathToFilename[filepath] + '.js';
